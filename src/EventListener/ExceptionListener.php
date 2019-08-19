d8106ee76d39342781c14efed5e44ae2707d4244
karpikovas <karpikovasophie@gmail.com>
2019-08-19 12:00:24 +0000

    Save changes

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -4,8 +4,10 @@
 namespace App\EventListener;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class ExceptionListener extends AbstractController
@@ -13,16 +15,23 @@ class ExceptionListener extends AbstractController
   public function onKernelException(ExceptionEvent $event)
   {
     $exception = $event->getException();
-    $error = $exception->getCode();
+    $errorCode = Response::HTTP_INTERNAL_SERVER_ERROR; // 500
+    $errorMessage = $exception->getMessage();
+
 
 
     if ($exception instanceof HttpExceptionInterface) {
-      $error = $exception->getStatusCode();
-    } else {
-      $error = Response::HTTP_INTERNAL_SERVER_ERROR;
+      $errorCode = $exception->getStatusCode();
     }
 
-    $response = new Response($this->renderView('error/error.html.twig', ['error' => $error]));
+    if ($event->getRequest()->isXmlHttpRequest()) {
+      $response = new Response($this->renderView('error/json', ['code' => $errorCode, 'message' => $errorMessage]));
+    } else {
+      $response = new Response($this->renderView('error/error.html.twig', ['code' => $errorCode, 'message' => $errorMessage]));
+    }
+
+    $response->setStatusCode($errorCode);
+
     $event->setResponse($response);
   }
 }
